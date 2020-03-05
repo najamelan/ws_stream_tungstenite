b3bb0912631fbd751abdc181b5618fba7ae135e8
Naja Melan <najamelan@autistici.org>
2020-03-05 21:19:57 +0100

    Add a test that uses async-std rather than tokio.

--- tests/ping_pong_async_std.rs
+++ tests/ping_pong_async_std.rs
@@ -1,3 +1,5 @@
+#![type_length_limit="1597428"] // https://github.com/rust-lang/rust/issues/64496
+
 // Verify whether we handle ping and pong frames correctly
 // Note that they can hold data, but as we provide AsyncRead/AsyncWrite, we generally don't
 // want to produce any data on ping/pong. Just on binary messages.
@@ -8,30 +10,30 @@
 //
 use
 {
-	ws_stream_tungstenite :: { *                                                      } ,
-	futures               :: { StreamExt, SinkExt, future::join                       } ,
-	futures_codec         :: { LinesCodec, Framed                                     } ,
-	tokio                 :: { net::{ TcpListener }                                   } ,
-	async_tungstenite     :: { accept_async, tokio::{ connect_async, TokioAdapter }   } ,
-	url                   :: { Url                                                    } ,
+	ws_stream_tungstenite :: { *                                      } ,
+	futures               :: { StreamExt, SinkExt, future::join       } ,
+	futures_codec         :: { LinesCodec, Framed                     } ,
+	async_std             :: { net::{ TcpListener }                   } ,
+	async_tungstenite     :: { accept_async, async_std::connect_async } ,
+	url                   :: { Url                                    } ,
 
 	log :: { * } ,
 };
 
 
-#[ tokio::test ]
+#[ async_std::test ]
 //
-async fn ping_pong()
+async fn ping_pong_async_std()
 {
 	// flexi_logger::Logger::with_str( "ping_pong=trace, tungstenite=trace, async_tungstenite=trace, ws_stream_tungstenite=trace, tokio=warn" ).start().expect( "flexi_logger");
 
 	let server = async
 	{
-		let mut socket      = TcpListener::bind( "127.0.0.1:3015" ).await.expect( "bind to port" );
+		let     socket      = TcpListener::bind( "127.0.0.1:3015" ).await.expect( "bind to port" );
 		let mut connections = socket.incoming();
 
 		let tcp_stream = connections.next().await.expect( "1 connection" ).expect( "tcp connect" );
-		let s          = accept_async(TokioAdapter(tcp_stream)).await.expect("Error during the websocket handshake occurred");
+		let s          = accept_async( tcp_stream ).await.expect("Error during the websocket handshake occurred");
 		let server     = WsStream::new( s );
 
 		let mut framed = Framed::new( server, LinesCodec {} );
--- todo.md
+++ todo.md
@@ -1,5 +1,7 @@
 # TODO
 
-- get rid of the ok().and_then
-- test with async-std
+- documentation
+- CI
+- clean up ws_stream_wasm
+- check crate template for changes.
 - Look into AsyncBufReader which allows the io to allocate the buffer as opposed to AsyncRead, since tungstenite allocates a Vec for binary. This might allow avoiding  double allocation.
